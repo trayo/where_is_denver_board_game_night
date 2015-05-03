f13103b4733b5382d8c52ef6c2cdcedca12fcd38
Travis Yoder <trayoda@gmail.com>
2015-05-03 08:46:46 -0600

    made a server.rb that displays the index and shows the date and location

---
+++ app/server.rb
@@ -0,0 +1,12 @@
+require "sinatra"
+require_relative "date_fetcher"
+
+module BoardGameNight
+
+  class Server < Sinatra::Base
+    get '/' do
+      l = DateFetcher.fetch.first
+      erb :index, :locals => {date: l.first, location: l.last}
+    end
+  end
+end
