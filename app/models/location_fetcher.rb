82c368219fb8645dd51f32848f3096a1fccece0d
Travis Yoder <trayoda@gmail.com>
2015-05-04 15:07:26 -0600

    changed the name of update to match destroy
    added a destroy method to destroy all locations
    
    added some console output to show what locations
    are being added or destroyed

--- app/models/location_fetcher.rb
+++ app/models/location_fetcher.rb
@@ -4,13 +4,17 @@ module BoardGameNight
 
   class LocationFetcher
 
-    def self.update
+    def self.update_locations
       new(fetch_dates_and_locations).create_locations
     end
 
     def self.fetch_dates_and_locations
       Nokogiri::HTML(open("http://www.reddit.com/r/Denver/wiki/wednesdaymeetup"))
-        .at_css("#wiki_tentative_schedule").next_element.children.map(&:text)
+      .at_css("#wiki_tentative_schedule").next_element.children.map(&:text)
+    end
+
+    def self.destroy_locations
+      Location.destroy_all
     end
 
     attr_reader :lines
@@ -22,14 +26,26 @@ module BoardGameNight
     def create_locations
       @lines.each do |date, location|
         if after_today_and_doesnt_exist?(date)
-          Location.create(date: date, name: location)
+          l = Location.create(date: date, name: location)
+          puts "Location #{l.name} on #{l.date} created!"
         elsif before_today_and_does_exist?(date)
-          Location.find_by(date: date).destroy
+          l = Location.find_by(date: date).destroy
+          puts "Location #{l.name} destroyed!"
+        else
+          print_date_before_today_or_already_exists(date, location)
         end
       end
     end
 
     private
+
+    def print_date_before_today_or_already_exists(date, location)
+      if date < Date.today
+        puts "Already happened #{location} on #{date}."
+      else
+        puts "Already added #{location} on #{date}."
+      end
+    end
 
     def after_today_and_doesnt_exist?(date)
       date > Date.today && !Location.exists?(date: date)
