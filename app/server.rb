d520f9f63c401ca7e08cb085381b6c8126856d2b
Travis Yoder <trayoda@gmail.com>
2015-05-03 20:58:33 -0600

    updated root_path
    added comment for a future feature of auto directions

--- app/server.rb
+++ app/server.rb
@@ -1,9 +1,13 @@
 module BoardGameNight
   class Server < Sinatra::Base
     get '/' do
-      l = DateFetcher.update.first
+      @location = Location.first
       erb :index, :locals => {date: l.first, location: l.last}
     end
+
+    # get '/directions' do
+    #   automatically open google maps directions
+    # end
 
     not_found do
       erb :error
