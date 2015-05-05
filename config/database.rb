2a6b5b405a9c608d51605d1e7dbb469c828c4990
Travis Yoder <trayoda@gmail.com>
2015-05-05 17:14:23 -0600

    removed the puts. PRODUCTION WORKS

--- config/database.rb
+++ config/database.rb
@@ -2,7 +2,6 @@
 database_name = "game-night-#{BoardGameNight::Server.environment}"
 db = URI.parse(ENV['DATABASE_URL'] || "postgres://localhost/#{database_name}")
 
-puts "\n\n\n#{db.path}\n\n\n"
 # connect ActiveRecord with the current database
 ActiveRecord::Base.establish_connection(
   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
