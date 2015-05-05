1f3aca88171a432ea0ac8716929c0012d63386c6
Travis Yoder <trayoda@gmail.com>
2015-05-05 13:41:32 -0600

    finished up the server. it checks that the location exists before it tries to render the view

--- app/server.rb
+++ app/server.rb
@@ -1,17 +1,31 @@
 module BoardGameNight
   class Server < Sinatra::Base
     get '/' do
-      l = Location.first
-      erb :index, locals: {date: l.date, name: l.name}
+      location = Location.first
+      if location
+        erb :index, locals: {date: location.date, name: location.name}
+      else
+        erb :error, locals: {message: location_not_found}
+      end
     end
 
     get '/directions' do
-      location = Location.first.name.gsub(" ", "+")
-      redirect "https://www.google.com/maps/dir/Current+Location/#{location}"
+      if Location.first
+        location = Location.first.name.gsub(" ", "+")
+        redirect "https://www.google.com/maps/dir/Current+Location/#{location}"
+      else
+        erb :error, locals: {message: location_not_found}
+      end
     end
 
     not_found do
       erb :error
+    end
+
+    private
+
+    def location_not_found
+      "Oops! I didn't find the next location."
     end
   end
 end
