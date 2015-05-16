828e63d3002111351205f254091ec98f93540934
Travis Yoder <trayoda@gmail.com>
2015-05-16 13:41:54 -0600

    added address and url to location

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516193924) do
+ActiveRecord::Schema.define(version: 20150516194034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20150516193924) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "name"
+    t.string   "address"
+    t.string   "url"
   end
 
 end
