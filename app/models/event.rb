8c8375bd964a68e6c2763475d34ad60817fe6488
Travis Yoder <trayoda@gmail.com>
2015-05-20 12:27:44 -0600

    added a default_scope to event to order by date
    added helper methods for getting to the tied
    locations data

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,14 @@
 module BoardGameNight
   class Event < ActiveRecord::Base
     belongs_to :location
+    default_scope { order(:date) }
+
+    def name
+      location.name
+    end
+
+    def address
+      location.address
+    end
   end
 end
