eee69e45876d17af7c9be2485e4a228a6cbc9010
Travis Yoder <trayoda@gmail.com>
2016-05-30 20:15:23 -0700

    fix formatting and clarity in location_fetcher tests

--- test/models/location_fetcher_test.rb
+++ test/models/location_fetcher_test.rb
@@ -14,10 +14,6 @@ module BoardGameNight
       Event.destroy_all
     end
 
-    def days_from_now(num)
-      Time.now.advance(days: num).to_date
-    end
-
     def test_it_can_fetch_from_reddit
       dates_and_locations = []
 
@@ -34,13 +30,14 @@ module BoardGameNight
         "April 11th 2200: Tabletop Day at Irish Snug",
         "April 15th 2200: Diebolt Brewing"
       ]
-
       expected = {
         Date.parse("April 11th 2200") => "Tabletop Day at Irish Snug",
         Date.parse("April 15th 2200") => "Diebolt Brewing"
       }
 
-      assert_equal expected, LocationFetcher.new(lines).lines
+      result = LocationFetcher.new(lines).lines
+
+      assert_equal expected, result
     end
 
     def test_it_creates_new_events_and_locations
@@ -53,6 +50,7 @@ module BoardGameNight
 
       assert_equal 2, Location.count
       assert_equal "Diebolt Brewing", Location.last.name
+
       assert_equal 2, Event.count
       assert_equal days_from_now(9), Event.last.date
     end
@@ -60,8 +58,8 @@ module BoardGameNight
     def test_it_uses_existing_events_and_locations
       l = Location.create(name: "Super Smash Brewery")
       l.events << Event.create(date: days_from_now(3))
-
       lines = ["#{days_from_now(3)}: Super Smash Brewery"]
+
       LocationFetcher.new(lines).create_events_and_locations
 
       assert_equal 1, Location.count
@@ -71,8 +69,8 @@ module BoardGameNight
     def test_it_updates_an_existing_location
       l = Location.create(name: "TBD/TBA")
       l.events << Event.create(date: days_from_now(5))
-
       lines = ["#{days_from_now(5)}: Die Hard Brews"]
+
       LocationFetcher.new(lines).create_events_and_locations
 
       assert_equal 1, Location.count
@@ -107,6 +105,10 @@ module BoardGameNight
     end
 
     private
+
+      def days_from_now(num)
+        Time.now.advance(days: num).to_date
+      end
 
       def silence_stdout
         @original_stdout = $stdout
